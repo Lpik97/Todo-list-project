437371161d71e0cbee2fa2be3f438b62683ad810
Lpik97 <Lpik97@users.noreply.github.com>
2023-07-19 16:22:35 +0200

    fix(project form properties names)
    Minor properties names were modified.

--- modules/projectform.js
+++ modules/projectform.js
@@ -1,8 +1,8 @@
 const PROJECT_FORM = () => {
 
-    const ADD_PROJECT_BUTTON = document.getElementById('add-project-button');
+    const ADD_PROJECT = document.getElementById('add-project');
 
-    ADD_PROJECT_BUTTON.addEventListener('click', () => {
+    ADD_PROJECT.addEventListener('click', () => {
 
         const LOWER_MAIN_SECTION = document.querySelector('.lower-content');
 
@@ -10,28 +10,12 @@ const PROJECT_FORM = () => {
 
         FORM.innerHTML = `
         <form>
-            <input type="text" name="title" id="title" placeholder="Task name">
-            <input type="text" name="description" id="description" placeholder="Description">
-            <fieldset class="interactive-inputs">
-                <div class="due-date-input">
-                    <legend>Due date:</legend>
-                    <input type="date" name="dueDate" id="dueDate">
-                </div>
-                <div class="priority-input">
-                    <legend>Priority</legend>
-                    <select name="priority" id="priority">
-                        <option value="High">High</option>
-                        <option value="Medium">Medium</option>
-                        <option value="Low">Low</option>
-                    </select>   
-                </div>
-            </fieldset>
+            <input type="text" name="title" id="title" placeholder="Project name">
             <hr>
             <div class="lower-form">
-                <div class="current-project-display"></div>
                 <div class="buttons-area">
                     <button class="interactive-button" id="cancel-button">Cancel</button>
-                    <button class="interactive-button" id="add-todo-button">Add task</button>    
+                    <button class="interactive-button" id="add-project-button">Add project</button>    
                 </div>
             </div>
         </form>
