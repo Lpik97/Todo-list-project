1a43285866ba6c4d2684950273ed1d1bc3f67b72
Lpik97 <Lpik97@users.noreply.github.com>
2023-07-21 09:21:48 +0200

    feat(display project function)
    This function will display every project and their todos based on the project index in the projects array.

---
+++ modules/displayproject.js
@@ -0,0 +1,25 @@
+import PROJECTS_ARRAY from "./projectsarray.js";
+
+const DISPLAY_PROJECT = () => {
+
+    const CREATED_PROJECTS = document.getElementById(created-projects);
+
+    CREATED_PROJECTS.innerHTML = '';
+
+    PROJECTS_ARRAY.forEach((PROJECT, INDEX) => {
+
+        const PROJECT_ELEMENT = document.createElement('div');
+
+        PROJECT_ELEMENT.innerText = PROJECT.title;
+
+        PROJECT_ELEMENT.addEventListener('click', () => {
+
+            DISPLAY_TODO(INDEX);
+
+        });
+
+    });
+
+};
+
+export default DISPLAY_PROJECT;
