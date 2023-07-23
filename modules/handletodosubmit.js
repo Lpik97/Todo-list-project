f84e550f2f967f8a0ca0f813151c0edae2732e49
Lpik97 <Lpik97@users.noreply.github.com>
2023-07-23 08:42:59 +0200

    feat(handle todo submit function)
    This function works in the same way as handle project submit, first preventing the defaul behaviour of the form submit, then creating a new todo element using the form inputs, adding the todo to the todo array and invoking the display todo function.

---
+++ modules/handletodosubmit.js
@@ -0,0 +1,29 @@
+import ADD_NEW_TODO from "./addnewtodo.js";
+import DISPLAY_TODO from "./displaytodo.js";
+import TODO_FACTORY from "./todofactory.js";
+
+function HANDLE_TODO_SUBMIT(FORM) {
+
+    FORM.addEventListener('submit', (event) => {
+        
+        event.preventDefault();
+
+        const TODO_TITLE = FORM.querySelector('#title').value;
+
+        const TODO_DESCRIPTION = FORM.querySelector('#description').value;
+
+        const TODO_DUE_DATE = FORM.querySelector('#dueDate').value;
+
+        const TODO_PRIORITY = FORM.querySelector('#priority').value;
+
+        const NEW_TODO = TODO_FACTORY(TODO_TITLE, TODO_DESCRIPTION, TODO_DUE_DATE, TODO_PRIORITY);
+
+        ADD_NEW_TODO(NEW_TODO);
+
+        DISPLAY_TODO();
+
+    });
+
+};
+
+export default HANDLE_TODO_SUBMIT
