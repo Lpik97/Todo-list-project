43b71e6723edb40dd19a52ae4376d38954d77944
Lpik97 <Lpik97@users.noreply.github.com>
2023-07-23 08:08:09 +0200

    feat(display project function)
    This function loops through the array of created projects and adds an click event listener to each of them invoking the open project function.

--- modules/displayproject.js
+++ modules/displayproject.js
@@ -1,24 +1,25 @@
 import PROJECTS_ARRAY from "./projectsarray.js";
+import OPEN_PROJECT from "./openproject.js";
 
 const DISPLAY_PROJECT = () => {
 
-    const CREATED_PROJECTS = document.getElementById(created-projects);
+    const CREATED_PROJECTS = document.getElementById('created-projects');
 
     CREATED_PROJECTS.innerHTML = '';
 
-    PROJECTS_ARRAY.forEach((PROJECT, INDEX) => {
+    PROJECTS_ARRAY.forEach(PROJECT => {
 
         const PROJECT_ELEMENT = document.createElement('div');
 
         PROJECT_ELEMENT.innerText = PROJECT.title;
 
+        CREATED_PROJECTS.appendChild(PROJECT_ELEMENT);
+
         PROJECT_ELEMENT.addEventListener('click', () => {
 
-            DISPLAY_TODO(INDEX);
+            OPEN_PROJECT(PROJECT);
 
         });
-
-        CREATED_PROJECTS.appendChild(PROJECT_ELEMENT);
 
     });
 
