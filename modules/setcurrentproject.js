fd50861ef4bdd167f2d682be22bf23934f6c951a
Lpik97 <Lpik97@users.noreply.github.com>
2023-07-24 10:48:07 +0200

    fix(typo)
    Small typo fixed.

--- modules/setcurrentproject.js
+++ modules/setcurrentproject.js
@@ -2,7 +2,7 @@ const SET_CURRENT_PROJECT = () => {
 
   const CREATED_PROJECTS = document.querySelectorAll('#created-projects li.project');
 
-  CREATED_PROJECTS.forEach(PROJECT => {
+  CREATED_PROJECTS.forEach((PROJECT) => {
 
     PROJECT.addEventListener('click', () => {
 
