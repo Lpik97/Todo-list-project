49a4eef3610ab030df1d4096a6d6a7e7b060a9c9
Lpik97 <Lpik97@users.noreply.github.com>
2023-07-31 10:46:51 +0200

    feat(clean content function)
    This function will clear all the inner html content from the upper and lower content sections.

---
+++ modules/cleancontent.js
@@ -0,0 +1,13 @@
+const CLEAN_CONTENT = () => {
+
+    const UPPER_CONTENT = document.querySelector('.upper-content');
+
+    const LOWER_CONTENT = document.querySelector('.lower-content');
+    
+    UPPER_CONTENT.innerHTML = '';
+    
+    LOWER_CONTENT.innerHTML = '';
+
+};
+
+export default CLEAN_CONTENT;
