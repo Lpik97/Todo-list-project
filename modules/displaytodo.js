70ea3070d243c03364b49525c17252ebbc53ca83
Lpik97 <Lpik97@users.noreply.github.com>
2023-08-01 11:40:02 +0200

    feat(several new features in the display todo function)
    The function will set an attribute of data-todo-id using the TODO.id to the TODO ELEMENT. Once clicked on, the SELECTED TODO will be selected searching inside the SELECTED PROJECT.TODOS array with its data todo id and then that same SELECTED TODO will be used as a parameter for OPEN TODO function.

--- modules/displaytodo.js
+++ modules/displaytodo.js
@@ -1,20 +1,28 @@
-const DISPLAY_TODO = () => {
+const DISPLAY_TODO = (SELECTED_PROJECT) => {
 
     const TODOS_LIST = document.querySelector('.upper-content');
 
     TODOS_LIST.innerHTML = '';
 
-    TODOS_LIST.forEach(TODO => {
+    SELECTED_PROJECT.TODOS.forEach(TODO => {
 
         const TODO_ELEMENT = document.createElement('div');
 
         TODO_ELEMENT.innerText = TODO.title;
+
+        TODO_ELEMENT.setAttribute('data-project-id', TODO.id);
 
         TODOS_LIST.appendChild(TODO_ELEMENT);
 
         TODO_ELEMENT.classList.add('todo');
 
         TODO_ELEMENT.addEventListener('click', () => {
+
+            const TODO_ELEMENT_ID = TODO_ELEMENT.getAttribute('data-todo-id');
+
+            const SELECTED_TODO = SELECTED_PROJECT.TODOS.find(TODO => TODO.id === TODO_ELEMENT_ID);
+
+            OPEN_TODO(SELECTED_TODO);
 
         });
 
