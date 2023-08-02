cc7c4106b32742452cd0ecf7746048a4b8dc3500
Lpik97 <Lpik97@users.noreply.github.com>
2023-08-02 11:42:13 +0200

    feat(deleteproject, deletetodo functions invoked, small change)
    The corresponding function for each module was invoked with the correct parameters. A small change in the innerhtml of the todos and projects elements was effectuated.

--- modules/displayproject.js
+++ modules/displayproject.js
@@ -1,5 +1,6 @@
 import PROJECTS_ARRAY from "./projectsarray.js";
 import OPEN_PROJECT from "./openproject.js";
+import DELETE_PROJECT from "./deleteproject.js";
 
 const DISPLAY_PROJECT = () => {
 
@@ -12,11 +13,11 @@ const DISPLAY_PROJECT = () => {
         const PROJECT_ELEMENT = document.createElement('li');
 
         PROJECT_ELEMENT.innerHTML = `
-        <h1 class=todo-element-title>${PROJECT.title}<h1>
+        <h1 class=todo-element-title>${PROJECT.title}</h1>
         <button class="functionality-button" id="delete-button">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                    <path d="M261-120q-24.75 0-42.375-17.625T201-180v-570h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm438-630H261v570h438v-570ZM367-266h60v-399h-60v399Zm166 0h60v-399h-60v399ZM261-750v570-570Z"/>
-                </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                <path d="M261-120q-24.75 0-42.375-17.625T201-180v-570h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm438-630H261v570h438v-570ZM367-266h60v-399h-60v399Zm166 0h60v-399h-60v399ZM261-750v570-570Z"/>
+            </svg>
         </button>
         `;
 
@@ -25,6 +26,8 @@ const DISPLAY_PROJECT = () => {
         CREATED_PROJECTS.appendChild(PROJECT_ELEMENT);
 
         PROJECT_ELEMENT.classList.add('project');
+
+        DELETE_PROJECT();
 
         PROJECT_ELEMENT.addEventListener('click', () => {
 
