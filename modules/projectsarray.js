ebecdb17c817b34fb5b49ebd1030549cc2209233
Lpik97 <Lpik97@users.noreply.github.com>
2023-08-03 20:35:15 +0200

    feat(projects imported from local storage)
    The projects array will import the data from the local storage as default or create a new empty array of projects.

--- modules/projectsarray.js
+++ modules/projectsarray.js
@@ -1,3 +1,5 @@
-let ProjectsArray =  [];
+import LOCAL_STORAGE_PROJECTS_KEY from "./localstorage.js";
 
-export default ProjectsArray;
+let projectsArray = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECTS_KEY)) || [];
+
+export default projectsArray;
