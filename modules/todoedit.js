4ba7641162cf53bf20d86f35ffaab34ae033db37
Lpik97 <Lpik97@users.noreply.github.com>
2023-08-03 20:04:15 +0200

    feat(todo edit function module)
    This function uses expanded todo, selected todo and selected project as parameters. First adding an event listener to the edit expanded todo button, once clicked it will clear all the content, add a new form to edit the todo and use the inputs to change the currently existing todo object. Then it will call clean content again and open todo with the newly modified selected todo.

---
+++ modules/todoedit.js
@@ -0,0 +1,71 @@
+import CLEAN_CONTENT from "./cleancontent.js";
+import OPEN_TODO from "./opentodo.js";
+
+const TODO_EDIT = (EXPANDED_TODO, SELECTED_TODO, SELECTED_PROJECT) => {
+
+    const EDIT_EXPANDED_BUTTON = EXPANDED_TODO.querySelector('#edit-expanded-todo');
+
+    EDIT_EXPANDED_BUTTON.addEventListener('click', () => {
+
+        const FORM_SECTION = document.querySelector('.lower-content');
+
+        CLEAN_CONTENT();
+
+        const FORM = document.createElement('form');
+
+        FORM.innerHTML = `
+        <form>
+            <input type="text" name="title" id="title" placeholder="${SELECTED_TODO.title}" required>
+            <input type="text" name="description" id="description" placeholder="${SELECTED_TODO.description}" required>
+            <fieldset class="interactive-inputs">
+                <div class="due-date-input">
+                    <legend>Due date:</legend>
+                    <input type="date" name="dueDate" id="dueDate" required>
+                </div>
+                <div class="priority-input">
+                    <legend>Priority</legend>
+                    <select name="priority" id="priority" required>
+                        <option value="High">High</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Low">Low</option>
+                    </select>   
+                </div>
+            </fieldset>
+            <hr>
+            <div class="lower-form">
+                <div class="current-project-display">${SELECTED_PROJECT.title}</div>
+                <div class="buttons-area">
+                    <button class="interactive-button" id="cancel-button">Cancel</button>
+                    <button class="interactive-button" id="add-todo-button">Edit task</button>    
+                </div>
+            </div>
+        </form>
+        `;
+
+        FORM_SECTION.appendChild(FORM);
+
+        FORM.classList.add('edit-todo-form');
+
+        FORM.addEventListener('submit', (event) => {
+            
+            event.preventDefault();
+
+            SELECTED_TODO.title = FORM.querySelector('#title').value;
+
+            SELECTED_TODO.description = FORM.querySelector('#description').value;
+
+            SELECTED_TODO.dueDate = FORM.querySelector('#dueDate').value;
+
+            SELECTED_TODO.priority = FORM.querySelector('#priority').value;
+
+            CLEAN_CONTENT();
+
+            OPEN_TODO(SELECTED_TODO, SELECTED_PROJECT);
+        
+        });
+
+    });
+
+};
+
+export default TODO_EDIT;
