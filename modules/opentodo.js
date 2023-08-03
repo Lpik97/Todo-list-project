811585970a17631ba7023648d3f9a61d35be9f38
Lpik97 <Lpik97@users.noreply.github.com>
2023-08-03 19:04:42 +0200

    feat(expanded todo inner html)
    The inner html of the expanded todo element was completly restructured.

--- modules/opentodo.js
+++ modules/opentodo.js
@@ -10,10 +10,21 @@ const OPEN_TODO = (SELECTED_TODO) => {
 
     EXPANDED_TODO.innerHTML = `
     <div class="expanded-todo">
-        <h1 class="expanded-todo-title">${SELECTED_TODO.title}</h1>
+        <div class="expanded-todo-upper-container">
+            <h1 class="expanded-todo-title">${SELECTED_TODO.title}</h1>
+            <button class="functionality-button" id="edit-expanded-todo">
+                <svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30">
+                    <path d="M180-180h44l443-443-44-44-443 443v44Zm614-486L666-794l42-42q17-17 42-17t42 17l44 44q17 17 17 42t-17 42l-42 42Zm-42 42L248-120H120v-128l504-504 128 128Zm-107-21-22-22 44 44-22-22Z"/>
+                </svg>
+            </button>
+        </div>
+        <hr>
         <p class="expanded-todo-description">${SELECTED_TODO.description}</p>
-        <div class="expanded-todo-dueDate">${SELECTED_TODO.dueDate}</div>
-        <div class="expanded-todo-priority">${SELECTED_TODO.priority}</div>
+        <div class="expanded-todo-bottom-container">
+            <div class="expanded-todo-dueDate">Due date: ${SELECTED_TODO.dueDate}</div>
+            <div class="expanded-todo-priority">${SELECTED_TODO.priority} priority</div>
+        </div>
+        
     </div>
     `;
 
