eb49bbe0e614421a5909fbe42bbcd43605102d0f
Lpik97 <Lpik97@users.noreply.github.com>
2023-08-04 12:11:35 +0200

    fix(expanded todo button id name changed)

--- modules/opentodo.js
+++ modules/opentodo.js
@@ -13,7 +13,7 @@ const OPEN_TODO = (SELECTED_TODO, SELECTED_PROJECT) => {
     <div class="expanded-todo">
         <div class="expanded-todo-upper-container">
             <h1 class="expanded-todo-title">${SELECTED_TODO.title}</h1>
-            <button class="functionality-button" id="edit-expanded-todo">
+            <button id="edit-expanded-todo-button">
                 <svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30">
                     <path d="M180-180h44l443-443-44-44-443 443v44Zm614-486L666-794l42-42q17-17 42-17t42 17l44 44q17 17 17 42t-17 42l-42 42Zm-42 42L248-120H120v-128l504-504 128 128Zm-107-21-22-22 44 44-22-22Z"/>
                 </svg>
