475f6e478828d67dad3b5728b7fb4e36ba052cc4
Lpik97 <Lpik97@users.noreply.github.com>
2023-08-05 19:33:46 +0200

    feat(local storage imported and invoked)
    The local storage will be called as the first property, then if there are no elements inside it, the three default projects will be created.

--- modules/projectsarray.js
+++ modules/projectsarray.js
@@ -1,4 +1,6 @@
-let projectsArray = [
+import LOCAL_STORAGE_PROJECT_KEY from "./projectslocalstorage.js";
+
+let projectsArray = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || [
     { title: "Inbox", id: "1", TODOS: [] },
     { title: "Today", id: "2", TODOS: [] },
     { title: "Upcoming", id: "3", TODOS: [] },
