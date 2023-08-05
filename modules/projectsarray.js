c67a316c66fdff3225cf5f79982c7992d6789b09
Lpik97 <Lpik97@users.noreply.github.com>
2023-08-05 19:19:31 +0200

    fix(projects Array default objects)
    The three default projects are Inbox, Today and Upcoming. All the rest of the objects will be added by the user.

--- modules/projectsarray.js
+++ modules/projectsarray.js
@@ -1,5 +1,7 @@
-import LOCAL_STORAGE_PROJECT_KEY from "./projectslocalstorage.js"
-
-let projectsArray = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || [];
+let projectsArray = [
+    { title: "Inbox", id: "1", TODOS: [] },
+    { title: "Today", id: "2", TODOS: [] },
+    { title: "Upcoming", id: "3", TODOS: [] },
+  ];
 
 export default projectsArray;
