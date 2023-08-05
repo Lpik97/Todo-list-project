ab494fe8b4a353615d768fa50ec774f53918300d
Lpik97 <Lpik97@users.noreply.github.com>
2023-08-05 19:32:25 +0200

    feat(if statement for default and created projects)
    An if statement was added to the function that will check if wether the project has an id of 1, 2 or 3, being those the custom projects id or any other type of id, appending those projects in the correct section for each case.

--- modules/displayproject.js
+++ modules/displayproject.js
@@ -7,40 +7,72 @@ const DISPLAY_PROJECT = () => {
 
     const CREATED_PROJECTS = document.getElementById('created-projects');
 
+    const DEFAULT_PROJECTS = document.querySelector('#default-projects');
+
+    DEFAULT_PROJECTS.innerHTML = '';
+
     CREATED_PROJECTS.innerHTML = '';
 
     projectsArray.forEach(PROJECT => {
 
-        const PROJECT_ELEMENT = document.createElement('li');
+        if (PROJECT.id === '1' || PROJECT.id === '2' || PROJECT.id === '3') {
 
-        PROJECT_ELEMENT.innerHTML = `
-        <h1 class=todo-element-title>${PROJECT.title}</h1>
-        <button class="functionality-button" id="delete-button">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                <path d="M261-120q-24.75 0-42.375-17.625T201-180v-570h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm438-630H261v570h438v-570ZM367-266h60v-399h-60v399Zm166 0h60v-399h-60v399ZM261-750v570-570Z"/>
-            </svg>
-        </button>
-        `;
+            const PROJECT_ELEMENT = document.createElement('li');
 
-        PROJECT_ELEMENT.setAttribute('data-project-id', PROJECT.id);
+            PROJECT_ELEMENT.innerHTML = `<h1 class=todo-element-title>${PROJECT.title}</h1>`;
 
-        CREATED_PROJECTS.appendChild(PROJECT_ELEMENT);
+            PROJECT_ELEMENT.setAttribute('data-project-id', PROJECT.id);
 
-        PROJECT_ELEMENT.classList.add('project');
+            DEFAULT_PROJECTS.appendChild(PROJECT_ELEMENT);
 
-        UPDATE_PROJECTS();
+            PROJECT_ELEMENT.classList.add('project');
 
-        DELETE_PROJECT();
+            UPDATE_PROJECTS();
 
-        PROJECT_ELEMENT.addEventListener('click', () => {
+            PROJECT_ELEMENT.addEventListener('click', () => {
 
-            const PROJECT_ELEMENT_ID = PROJECT_ELEMENT.getAttribute('data-project-id');
+                const PROJECT_ELEMENT_ID = PROJECT_ELEMENT.getAttribute('data-project-id');
 
-            const SELECTED_PROJECT = projectsArray.find(PROJECT => PROJECT.id === PROJECT_ELEMENT_ID);
+                const SELECTED_PROJECT = projectsArray.find(PROJECT => PROJECT.id === PROJECT_ELEMENT_ID);
 
-            OPEN_PROJECT(SELECTED_PROJECT);
+                OPEN_PROJECT(SELECTED_PROJECT);
 
-        });
+            });
+
+        } else {
+
+            const PROJECT_ELEMENT = document.createElement('li');
+
+            PROJECT_ELEMENT.innerHTML = `
+            <h1 class=todo-element-title>${PROJECT.title}</h1>
+            <button class="functionality-button" id="delete-button">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                    <path d="M261-120q-24.75 0-42.375-17.625T201-180v-570h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm438-630H261v570h438v-570ZM367-266h60v-399h-60v399Zm166 0h60v-399h-60v399ZM261-750v570-570Z"/>
+                </svg>
+            </button>
+            `;
+
+            PROJECT_ELEMENT.setAttribute('data-project-id', PROJECT.id);
+
+            CREATED_PROJECTS.appendChild(PROJECT_ELEMENT);
+
+            PROJECT_ELEMENT.classList.add('project');
+
+            UPDATE_PROJECTS();
+
+            DELETE_PROJECT();
+
+            PROJECT_ELEMENT.addEventListener('click', () => {
+
+                const PROJECT_ELEMENT_ID = PROJECT_ELEMENT.getAttribute('data-project-id');
+
+                const SELECTED_PROJECT = projectsArray.find(PROJECT => PROJECT.id === PROJECT_ELEMENT_ID);
+
+                OPEN_PROJECT(SELECTED_PROJECT);
+
+            });
+
+        };
 
     });
 
